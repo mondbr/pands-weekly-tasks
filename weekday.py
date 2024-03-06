504ad0f0cb8b7221c545fd9b77a751758bfddf99
mondbr <mondab133@gmail.com>
2024-03-06 21:01:14 +0000

    weekly task 05: 2nd attempt - find out todays date and checking if it's  weekday

--- weekday.py
+++ weekday.py
@@ -4,7 +4,19 @@
 
 
 
-import datetime                             # https://www.w3schools.com/python/python_datetime.asp
-x = datetime.datetime.now()
 
-print ("Today is "+ x.strftime("%A"))       # https://www.w3schools.com/python/trypython.asp?filename=demo_datetime_strftime_a2
+print ("Is today weekday?")
+
+# Get the current day of the week (Monday is 0, Sunday is 6)
+import datetime  # https://www.w3schools.com/python/python_datetime.asp
+
+current_day = datetime.datetime.today().weekday()   # https://www.shecodes.io/athena/10185-how-to-check-what-day-of-the-week-it-is-in-python
+
+# Check if it is a weekday or weekend
+
+if current_day < 5: # Monday to Friday
+    print ("Yes, unfotunately today is a weekday :(")
+
+else: # 5th, 6th days - weekend
+    print ("It is a weekend, yay! :) ")
+
