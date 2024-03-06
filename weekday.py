302d0fa4294fa36590a177e645fe8a83a012c0f0
mondbr <mondab133@gmail.com>
2024-03-06 21:02:51 +0000

    weekly task 05 - 3rd attempt - correct typo

--- weekday.py
+++ weekday.py
@@ -5,12 +5,17 @@
 
 
 
-print ("Is today weekday?")
+print ("Is today a weekday?")
 
 # Get the current day of the week (Monday is 0, Sunday is 6)
-import datetime  # https://www.w3schools.com/python/python_datetime.asp
+import datetime     # https://www.w3schools.com/python/python_datetime.asp
+                    # provides classes for working with dates and times.
 
 current_day = datetime.datetime.today().weekday()   # https://www.shecodes.io/athena/10185-how-to-check-what-day-of-the-week-it-is-in-python
+                                                    # datetime.datetime.today() gets the current date and time.
+                                                    # weekday() is a method that returns the day of the week as an integer, where Monday is 0 and Sunday is 6.
+                                                    # current_day will be an integer representing the current day of the week. (Monday is 0, Sunday is 6)
+
 
 # Check if it is a weekday or weekend
 
