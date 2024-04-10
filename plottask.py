4f1b91f2a0795ebbd4a4aef72bef1c0bf7454932
mondbr <mondab133@gmail.com>
2024-04-10 23:18:32 +0100

    correcting week8 task

--- plottask.py
+++ plottask.py
@@ -1,4 +1,8 @@
-
+# plottask.py
+# author: Monika Dabrowska
+# Write a program called plottask.py that displays:
+# a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
+# and a plot of the function  h(x)=x3 in the range 0 to 10, 
 
 
 import numpy as np
@@ -7,22 +11,23 @@ import matplotlib.pyplot as plt
 # Generate 1000 random values from a normal distribution with mean 5 and standard deviation 2
 data = np.random.normal(5, 2, 1000)
 
-# Plot the histogram of the normal distribution
-plt.hist(data, bins=30, density=True, alpha=0.5, color='blue', label='Normal Distribution')
+# Plot histogram of the normal distribution
+plt.hist(data, alpha=0.9, color='blue', label='Normal Distribution', edgecolor="black")
 
-# Generate values for the function h(x) = x^3
+# Generate x values for the function h(x) = x^3
 x = np.linspace(0, 10, 100)
-y = x**3
+y = x ** 3
 
 # Plot the function h(x) = x^3
-plt.plot(x, y, color='red', label='$h(x) = x^3$')
+plt.plot(x, y, color='green', label='$h(x) = x^3$')
 
-# Add labels and legend
-plt.xlabel('X')
+# Add legend, labels, and title
+plt.legend()
+plt.xlabel('Values')
 plt.ylabel('Frequency / $h(x)$')
 plt.title('Histogram of Normal Distribution and Plot of $h(x) = x^3$')
-plt.legend()
-
-# Display the plot
-plt.grid(True)
 plt.show()
+
+
+# https://www.geeksforgeeks.org/how-to-plot-normal-distribution-over-histogram-in-python/
+
